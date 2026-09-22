2a3d46247441a010e32f5534909ce6130dee9265
LeninVasq <nehemiasvalencia31@gmail.com>
2026-09-21 17:54:56 -1100

    Inicio del sistema contable

---
+++ src/main/java/com/mycompany/sistema_contable/Sistema_Contable.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistema_contable;
+
+/**
+ *
+ * @author Nehemias Valencia
+ */
+public class Sistema_Contable {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
